ed1d33f413d87fa5be07d8c4014c7fad4220e4fe
Vikas Kaler <vikas.kalerv@gmail.com>
2017-09-09 15:37:58 -0400

    added ingredient model, updated th shopping list component to display the list and added form to add item to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+      new Ingredient('Tomatoes', 3),
+      new Ingredient('Paneer Bars', 4),
+      new Ingredient('Onions', 3)
+  ];
+  constructor() {
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
