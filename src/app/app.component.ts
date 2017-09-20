6d87290a619891d499c93fcad657aaa33b1d1bab
Vikas Kaler <vikas.kalerv@gmail.com>
2017-09-20 00:57:59 -0400

    added communication between the components using event, will changes it to services in future. Added the functionality of tabs on header using ngif, later will use Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  selectedTab = 'recipe';
+  updateNavigation(tab: string){
+    this.selectedTab = tab;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasRecieved = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
       new Recipe('Paneer Tikka',
           'Paneer tikka made of panner and green peppers',
@@ -19,9 +20,12 @@ export class RecipeListComponent implements OnInit {
           'http://www.vegrecipesofindia.com/wp-content/uploads/2014/08/tawa-paneer-tikka-recipe-2.jpg')
 
   ];
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+      ngOnInit() {
+      }
+    onRecipeGet(recipe: Recipe){
+        this.recipeWasRecieved.emit(recipe);
+    }
 
 }
